3551eaad52301742d8b70c9192b16b23ba698969
joel <joel@maraudersoftware.com>
2017-05-18 12:09:40 -0700

    NuGet v1.0.4, author fix

--- Indexer/Properties/AssemblyInfo.cs
+++ Indexer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Indexer")]
+[assembly: AssemblyTitle("IndexEngine")]
 [assembly: AssemblyDescription("Lightweight index engine written in C# using Sqlite")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Indexer")]
-[assembly: AssemblyCopyright("Copyright ©  2016 Marauder Software Inc")]
+[assembly: AssemblyCompany("Marauder Software Inc.")]
+[assembly: AssemblyProduct("IndexEngine")]
+[assembly: AssemblyCopyright("(c)2017 Marauder Software Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
--- IndexerTest/Properties/AssemblyInfo.cs
+++ IndexerTest/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IndexerTest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Test program for IndexEngine")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Marauder Software Inc.")]
 [assembly: AssemblyProduct("IndexerTest")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("(c)2017 Marauder Software Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
