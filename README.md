08c0b22a584971c3d977dcc23acfce9e2a37c1af
joel <joel@maraudersoftware.com>
2019-03-15 16:43:08 -0700

    NuGet v1.0.5, retarget to .NET Core 2.0 and .NET Framework

--- README.md
+++ README.md
@@ -1,34 +1,29 @@
-# index engine
+# IndexEngine
 
 [![][nuget-img]][nuget]
 
 [nuget]:     https://www.nuget.org/packages/IndexEngine
 [nuget-img]: https://badge.fury.io/nu/Object.svg
 
-simple indexer in C#
+IndexEngine is a simple indexer written in C# using Sqlite as a storage repository. Mono.Data.Sqlite was chosen specifically to ensure compatibility with Mono.  Copy sqlite3.dll to your output dir.
 
-Please refer to the test console application in the solution for testing the library. 
+As of release 1.0.5, IndexEngine is targeted to both .NET Core 2.0 and .NET Framework 4.5.2.
 
-## help or feedback
-first things first - do you need help or have feedback?  Contact me at joel at maraudersoftware.com dot com or file an issue here!
+## Help or feedback
 
-## description
-index engine is a simple indexer written in C# using Sqlite as a storage repository.  Mono.Data.Sqlite was chosen specifically to ensure compatibility with Mono.
+First things first - do you need help or have feedback?  Contact me at joel dot christner at gmail dot com or file an issue here!
 
-## performance and scale
-it's pretty quick :)  It hasn't been tested with huge document libraries or anything, so I'd recommend testing thoroughly before using in production. 
+## Performance and scale
 
-## using index engine
+It's pretty quick :)  It hasn't been tested with huge document libraries or anything, so I'd recommend testing thoroughly before using in production. 
+
+## Simple Example
 ```
 using Indexer;
-```
 
-```
-IndexEngine ie = new IndexEngine("idx.sqlite");
-```
+IndexEngine ie = new IndexEngine("idx.db");
 
-Indexing a document:
-```
+// Add a document
 Document d = new Document(
   "Title",				// i.e. Mark Twain
   "Description",		// i.e. A Great Book
@@ -39,17 +34,12 @@ Document d = new Document(
 );
 ie.SubmitDocument(d);		// async, returns immediately
 ie.SubmitDocumentSync(d);	// sync, returns after completion
-```
 
-Searching the index:
-```
+// Search the index
 List<string> terms = new List<string> { "some", "data" };
 List<Document> results = ie.SearchIndex(terms, null);
 foreach (Document d in results) Console.WriteLine(d.ToString());
-```
 
-Various other APIs:
-```
 // List number of threads actively indexing documents
 Console.WriteLine("Number of documents being indexed right now: " + ie.GetProcessingThreadsCount());
 
@@ -64,43 +54,4 @@ ie.DeleteDocumentByHandle("C:\\Documents\\MarkTwain.txt");
 Document d = RetrieveDocumentByGuid("abcd1234...");
 Document d = RetrieveDocumentByHandle("C:\\Documents\\MarkTwain.txt");
 bool b = IsHandleIndexed("C:\\Documents\\MarkTwain.txt");
-```
-
-## simple full app
-```
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Indexer;
-
-namespace sandbox
-{
-    public partial class Program
-    {
-        public static void Main(string[] args)
-        {
-            // initialize
-            IndexEngine ie = new IndexEngine("idx.sqlite");
-
-            // create document object
-            Document d = new Document(
-                "Title",
-                "Description",
-                "Some file system handle",
-                "The Internet",
-                "Joel",
-                Encoding.UTF8.GetBytes(
-                	"This is some data getting added into the index")
-            	);
-
-            // synchronously index the document
-            ie.SubmitDocumentSync(d);
-
-            // search the index and display the results
-            List<string> terms = new List<string>() { "index", "added" };
-            List<Document> res = ie.SearchIndex(terms, null);
-            if (res != null) foreach (Document c in res) Console.WriteLine(c.ToString());
-        }
-    }
-}
 ```
---
+++ TestNetFramework/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Indexer;
+
+namespace TestNetFramework
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string filename = "";
+                Console.Write("Filename: ");
+                filename = Console.ReadLine();
+                if (String.IsNullOrEmpty(filename)) return;
+                IndexEngine ie = new IndexEngine("index");
+                ie.ConsoleDebug = true;
+
+                bool runForever = true;
+                while (runForever)
+                {
+                    // Console.WriteLine("1234567890123456789012345678901234567890123456789012345678901234567890123456789");
+                    Console.WriteLine("Commands: q cls addfile addtext del threads search");
+                    string userInput = UserInputString("Command", false);
+
+                    Document doc = new Document();
+                    List<Document> results = new List<Document>();
+                    List<string> terms = new List<string>();
+                    List<string> activeDocs = new List<string>();
+                    string guid = "";
+
+                    switch (userInput.ToLower())
+                    {
+                        case "q":
+                        case "quit":
+                            runForever = false;
+                            break;
+
+                        case "c":
+                        case "cls":
+                            Console.Clear();
+                            break;
+
+                        case "addfile":
+                            doc = BuildFileDocument();
+                            ie.SubmitDocument(doc);
+                            break;
+
+                        case "addtext":
+                            doc = BuildStringDocument();
+                            ie.SubmitDocument(doc);
+                            break;
+
+                        case "del":
+                            guid = UserInputString("GUID", false);
+                            ie.DeleteDocumentByGuid(guid);
+                            break;
+
+                        case "threads":
+                            Console.WriteLine("Active document processing jobs: " + ie.GetProcessingThreadsCount());
+                            Console.WriteLine("Documents:");
+                            activeDocs = ie.GetProcessingDocumentsList();
+                            if (activeDocs != null && activeDocs.Count > 0)
+                            {
+                                foreach (string curr in activeDocs) Console.WriteLine("  " + curr);
+                            }
+                            else
+                            {
+                                Console.WriteLine("(null)");
+                            }
+                            break;
+
+                        case "search":
+                            terms = GatherTerms();
+                            if (terms == null || terms.Count < 1) break;
+                            results = ie.SearchIndex(terms, null);
+                            if (results == null || results.Count < 1)
+                            {
+                                Console.WriteLine("No results");
+                            }
+                            else
+                            {
+                                foreach (Document curr in results)
+                                {
+                                    Console.WriteLine(curr.ToString());
+                                }
+                            }
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                LogException(e);
+            }
+            finally
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Press ENTER to exit");
+                Console.ReadLine();
+            }
+        }
+
+        static void LogException(Exception e)
+        {
+            Console.WriteLine("================================================================================");
+            Console.WriteLine(" = Exception Type: " + e.GetType().ToString());
+            Console.WriteLine(" = Exception Data: " + e.Data);
+            Console.WriteLine(" = Inner Exception: " + e.InnerException);
+            Console.WriteLine(" = Exception Message: " + e.Message);
+            Console.WriteLine(" = Exception Source: " + e.Source);
+            Console.WriteLine(" = Exception StackTrace: " + e.StackTrace);
+            Console.WriteLine("================================================================================");
+        }
+
+        static Document BuildFileDocument()
+        {
+            Console.Write("File: ");
+            string file = Console.ReadLine();
+            if (String.IsNullOrEmpty(file)) return null;
+
+            byte[] data = File.ReadAllBytes(file);
+            if (data == null || data.Length < 1) return null;
+
+            Document ret = new Document(
+                UserInputString("Title", false),
+                UserInputString("Description", false),
+                file,
+                UserInputString("Source", false),
+                UserInputString("AddedBy", false),
+                data);
+
+            return ret;
+        }
+
+        static Document BuildStringDocument()
+        {
+            Document ret = new Document(
+                UserInputString("Title", false),
+                UserInputString("Description", false),
+                "String input",
+                UserInputString("Source", false),
+                UserInputString("AddedBy", false),
+                Encoding.UTF8.GetBytes(UserInputString("Data", false)));
+
+            return ret;
+        }
+
+        static List<string> GatherTerms()
+        {
+            List<string> ret = new List<string>();
+            while (true)
+            {
+                string term = UserInputString("Term", true);
+                if (!String.IsNullOrEmpty(term))
+                {
+                    ret.Add(term);
+                    continue;
+                }
+                break;
+            }
+            return ret;
+        }
+
+        static string UserInputString(string prompt, bool allowNull)
+        {
+            Console.Write(prompt + ": ");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (String.IsNullOrEmpty(input) && !allowNull) continue;
+                return input;
+            }
+        }
+    }
+}
